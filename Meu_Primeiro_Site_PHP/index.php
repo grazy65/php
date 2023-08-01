b45d0fca86f76b246ddf0e808c7f2a610afb856e
grazy65 <117763606+grazy65@users.noreply.github.com>
2023-08-01 16:22:41 -0300

    Add files via upload

---
+++ Meu_Primeiro_Site_PHP/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo "My first PHP script!";
+$txt = "PHP";
+echo "<br>Estou concatenado <br>"."I love $txt!";
+$x = 10;
+$y = 10.5;
+echo "<br>".$x + $y;
+
+?>
+
+</body>
+</html>
